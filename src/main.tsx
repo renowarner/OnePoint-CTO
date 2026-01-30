030c823105b903eec450be5963cfef02b82e3b40
Devlin <reno@renowarner.com>
2026-01-29 17:28:33 -0800

    Fix: Remove router basename for root domain deployment

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { BrowserRouter as Router } from 'react-router-dom'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Router basename="/renowarner.com">
+    <Router>
       <App />
     </Router>
   </StrictMode>,
